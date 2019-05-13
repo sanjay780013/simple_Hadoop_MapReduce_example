562f7a3951a38a56b2bacb68e00ecc6cae8a9c45
sanjay780013 <sanjaymaharjan18@gmail.com>
2019-05-13 22:00:56 +0000

    second commit

--- mapper.py
+++ mapper.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python
 import sys
+import string
+from sklearn.feature_extraction import stop_words
+ 
+stops = set(stop_words.ENGLISH_STOP_WORDS)
+
 
 # get all lines from stdin
 for line in sys.stdin:
+	
     # remove leading and trailing whitespace
-    line = line.strip()
+	line = line.strip().lower()
+
+# remove puntuation
+	line = line.translate(None, string.punctuation)
 
     # split the line into words; splits on any whitespace
-    words = line.split()
+	words = line.split()
 
     # output tuples (word, 1) in tab-delimited format
-    for word in words:
-        print '%s\t%s' % (word, "1")
+	for word in words:
+		if word not in stops:
+			print '%s\t%s' % (word, "1")
